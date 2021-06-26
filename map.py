60a560f9410b91b74a7f9ffddf883de0a2decfda
kyli0x <kyli0x@protonmail.ch>
2021-06-26 10:54:25 -0400

    changed map.html file name

--- map.py
+++ map.py
@@ -1,31 +1,38 @@
 import sys
 import io
+import requests
 import folium
 import pandas as pd
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout
-from PyQt5.QtWebEngineWidgets import QWebEngineView # pip install PyQtWebEngine
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-"""
-Folium in PyQt5
-"""
+
+# Folium inside Pyqt5
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Folium in PyQt Example')
+        self.setWindowTitle('SkateMap')
         self.window_width, self.window_height = 1600, 1200
         self.setMinimumSize(self.window_width, self.window_height)
 
         layout = QVBoxLayout()
         self.setLayout(layout)
 
+        # reading city data
         data = pd.read_csv('cities.csv', usecols=['name', 'lon', 'lat'])
 
+        # loading map & setting default start location
         m = folium.Map(
-                tiles='Stamen Terrain',
-        	zoom_start=6,
-        	location=[48.218871184761596, 11.624819877497147]
+            tiles='Stamen Terrain',
+            zoom_start=6,
+            location=[33.98813901349684, -118.46677927707837]
         )
 
+        folium.TileLayer
+
+
+
+        # goes through each location and adds marker to map
         for i in range(0,len(data)):
             folium.Marker(
                 location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
@@ -35,7 +42,7 @@ class MyApp(QWidget):
         # save map data to data object
         data = io.BytesIO()
         m.save(data, close_file=False)
-        m.save('map.html')
+        m.save('webmap.html')
 
         webView = QWebEngineView()
         webView.setHtml(data.getvalue().decode())
@@ -56,4 +63,4 @@ if __name__ == '__main__':
     try:
         sys.exit(app.exec_())
     except SystemExit:
-        print('Closing Window...')
+        print('Closing Program..')
